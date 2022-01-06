ca75db9ba1dc0da8bc86fe572b27ac0576d8407c
Ryoko <ryokonoda.1811@gmail.com>
2022-01-06 14:35:37 +0200

    Corrected outputs for normalized Bayesian models

--- codes/bayesian_GLM.R
+++ codes/bayesian_GLM.R
@@ -9,7 +9,7 @@ options(mc.cores=parallel::detectCores())  # use all available cores
 random_seed = 12244 # set random seed
 datafolder <- '../data/Modeling_Data/'
 resultfolder <- '../data/Results/Bayesian_GLM/'
-feature_type <- 'GLM' # GLM for random CV feature set, SGLM for spatial CV feature set
+feature_type <- 'SGLM' # GLM for random CV feature set, SGLM for spatial CV feature set
 normalize <- TRUE # TRUE if you want to normalize the data
 
 # specify file names for data
